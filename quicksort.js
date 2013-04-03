dbf798e8330c49fc0cf76ced067d9b9fde66a49e
Yusuke Nakayama <yusuke@yusuke-Aspire-S3-391.(none)>
2013-04-03 19:30:24 +0900

    first commit

---
+++ quicksort.js
@@ -0,0 +1,39 @@
+var quicksort = function(array,leftnum,rightnum){
+	this.rightarray = [];
+	this.leftarray  = [];
+	var indexleft   = leftnum;
+	var indexright  = rightnum;
+	var pivot;
+
+	pivot = (leftnum+rightnum)/2
+
+	while(1){
+		while(array[indexleft]<pivot)indexleft++;
+		while(array[indexright]>pivot)indexright--;
+	
+		if(indexleft>indexright) break;
+	
+		exchange(array[indexleft], array[indexright]);
+	}
+	if(leftnum < indexright) quicksort(array,leftnum,indexright);
+	if(indexleft < rightnum) quicksort(array,indexleft,rightnum);
+	
+	return array;
+}
+
+var exchange = function(left, right){
+	var change;
+	
+	change = left;
+	left   = right;
+	right  = change;
+}
+
+
+var array = [2,5,6,7,9,1,1,2,4,8];
+quicksort(array,0,array.length);
+
+for(var index = 0; index< array.length; index++){
+	console.log(array[index]);
+}
+	
