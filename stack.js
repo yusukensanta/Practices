9bcc4047311500b886df0ed9cac43f083c3aa0f0
Yusuke Nakayama <yusuke@yusuke-Aspire-S3-391.(none)>
2013-04-03 18:16:17 +0900

    Final Rivision - Compiled well part2

--- stack.js
+++ stack.js
@@ -1,27 +1,31 @@
 var Stack = function(){
 	this.array = [];
-	this.i = 0;
-	this.h = 1;
+	this.StackTop = 0;
 	this.push = function(x){	
-		this.array[this.i] = x;
-		this.i++;	
+		this.array[this.StackTop] = x;
+		this.StackTop++;	
 	}	
 	this.pop = function(){
-		var j = this.array.length - this.h;
-		this.h++;
-		return this.array[j];
+		this.StackTop--;
+		return this.array[this.StackTop];
 	}
 }
 function main(){
 	var s = new Stack();
 	s.push(1);
 	s.push(2);
-	s.push(9);
+	s.push(3);
+	s.push(4);
+	console.log(s.pop());
+	console.log(s.pop());
+	console.log(s.pop());
+	console.log(s.pop());
 	s.push(2);
+	console.log(s.pop());
+	s.push(9);
+	console.log(s.pop());
+	s.push(2);
+	console.log(s.pop());
 	
-	console.log(s.pop());
-	console.log(s.pop());
-	console.log(s.pop());
-	console.log(s.pop());
 }
 main();		
