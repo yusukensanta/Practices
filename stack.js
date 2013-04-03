607bea44ab1eea209fad4f32ffcf3fe31860fd4d
Yusuke Nakayama <yusuke@yusuke-Aspire-S3-391.(none)>
2013-04-03 16:09:04 +0900

    first commit

---
+++ stack.js
@@ -0,0 +1,18 @@
+var Stack = function(){
+	this.array = new Array();
+	this.push = function(x){	
+		this.array.push(x);	
+	}	
+	this.pop = function(){
+		this.array.pop();
+	}
+}
+
+var s = new Stack(){
+	s.push(1);
+	s.push(2);
+	
+	alert(s.pop());
+	alert(s.pop());
+}
+		
