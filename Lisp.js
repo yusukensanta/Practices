e4a8cd3961602ea3fef10b4dee1e57371bf4ab10
Yusuke Nakayama <yusuke@yusuke-Aspire-S3-391.(none)>
2013-04-09 14:42:00 +0900

    Change the Lexical Analysis

--- Lisp.js
+++ Lisp.js
@@ -6,6 +6,7 @@ repl.setPrompt(">>>");
 repl.prompt();
 
 repl.on("line",function(line){
+	console.log("here!");
 	L_Analysis(line);
 	repl.prompt();
 	});
@@ -16,10 +17,34 @@ repl.on("line",function(line){
 	});
 
 var L_Analysis = function(line){
-	var Term[];
-	var n = 0;
-	while(line.charAt[n] != null){
-		Term[n] == line.charAt[n];	
-		n++;
+	var Term = [];
+	var indexIn  = 0, indexOut = 0, character;
+	console.log("right here!");
+	while(line.charAt(indexIn) > line.charAt(line.length)){
+		character = line.charAt(indexIn);
+		
+		if(character.match(/[0-9]/) || character.match("-")){	
+			while(line.charAt(indexIn++) != " "){
+				character = character + line.charAt(indexIn);	
+			}
+			Term[indexOut] = character;		
+			console.log("Number");
+			indexOut++;				
+		}
+		else if(character.match(/[a-z]/) || character.match(/[A-Z]/)){
+			while(line.charAt(indexIn++) != " "){
+				character = character + line.charAt(indexIn)			
+			}
+			Term[indexOut] = character;
+			console.log("Character");
+			indexOut++;
+		}else if(character == " "){
+			indexIn++;
+			console.log("Brank");
+		}
+		else{
+			Term[indexOut] = character; indexIn++; indexOut++;
+			console.log("Symbol");
+		}
+	}
 }	
-	
